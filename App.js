4688fb58b8d62dfcd7d752ce47ebe2d4c9ba1c51
DelkTsai <DelkTsai@users.noreply.github.com>
2021-11-06 13:51:54 +0800

    add app file

---
+++ App.js
@@ -0,0 +1,30 @@
+// import logo from './logo.svg';
+import React,{Component} from 'react';
+import './App.css';
+import List from './List';
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//      <p>nihao</p>
+//     </div>
+//   );
+// }
+
+class App extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <List />
+      </div>
+    )
+  }
+}
+
+
+export default App;
