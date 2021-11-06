79406ac23467aabdbd7298e47c5ade397181afb9
caishuai <caishuai@aliyun.com>
2021-11-06 15:39:49 +0800

    bianji app file

--- App.js
+++ App.js
@@ -12,10 +12,14 @@ import List from './List';
 //   );
 // }
 
+//浪漫的定义几个字
 class App extends Component{
   constructor(props){
     super(props);
   }
+
+
+
 //演示一下,我叫张三丰,你叫李小二,她叫王五
   render(){
     return (
